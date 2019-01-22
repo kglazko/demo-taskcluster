cebd7d03c46800f498e7d0459dbe6abad439429f
kglazko <katglazko@gmail.com>
2019-01-22 15:33:41 -0800

    Update demo.py

--- tools/demo.py
+++ tools/demo.py
@@ -31,7 +31,7 @@ payload_source = {
   "properties": {
     "body": {
       "type": "string",
-      "description": “POSTING FROM TASKCLUSTER.”
+      "description": "The contents of the comment that were posted from TaskCluster.”
     }
   },
   "additionalProperties": false,
